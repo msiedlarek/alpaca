bde3f057ba6f3a6b36ca398fe75319e930f96e3b
Mikołaj Siedlarek <m.siedlarek@nctz.net>
2012-05-31 13:39:29 +0200

    more API tests

--- test.py
+++ test.py
@@ -4,9 +4,27 @@ import unittest
 import hmac
 import hashlib
 import datetime
+import threading
 import simplejson
 from alpaca import create_application
 from alpaca.tracker.models import User, Error
+
+def post_report(client, message, reporter, api_key):
+    message_json = simplejson.dumps(message)
+    return client.post(
+        path='/api/report',
+        data=message_json,
+        content_type='application/json',
+        headers={
+            'X-Alpaca-Reporter': reporter,
+            'X-Alpaca-Signature': generate_signature(
+                api_key, message_json
+            ),
+        }
+    )
+
+def generate_signature(api_key, message):
+    return hmac.new(api_key, message, hashlib.sha256).hexdigest()
 
 class ApiTestCase(unittest.TestCase):
 
@@ -38,18 +56,7 @@ class ApiTestCase(unittest.TestCase):
             cookies={},
             headers={}
         )
-        message_json = simplejson.dumps(message)
-        response = self.client.post(
-            path='/api/report',
-            data=message_json,
-            content_type='application/json',
-            headers={
-                'X-Alpaca-Reporter': 'test1',
-                'X-Alpaca-Signature': hmac.new(
-                    'test1_api_key', message_json, hashlib.sha256
-                ).hexdigest(),
-            }
-        )
+        response = post_report(self.client, message, 'test1', 'test1_api_key')
         self.assertEqual(response.status_code, 200)
         # MongoDB stores only 3-digit microseconds, so round the date for
         # comparsion.
@@ -99,18 +106,7 @@ class ApiTestCase(unittest.TestCase):
                 'header2': 'header2_value',
             }
         )
-        message_json = simplejson.dumps(message)
-        response = self.client.post(
-            path='/api/report',
-            data=message_json,
-            content_type='application/json',
-            headers={
-                'X-Alpaca-Reporter': 'test1',
-                'X-Alpaca-Signature': hmac.new(
-                    'test1_api_key', message_json, hashlib.sha256
-                ).hexdigest(),
-            }
-        )
+        response = post_report(self.client, message, 'test1', 'test1_api_key')
         self.assertEqual(response.status_code, 200)
         # MongoDB stores only 3-digit microseconds, so round the date for
         # comparsion.
@@ -165,18 +161,8 @@ class ApiTestCase(unittest.TestCase):
                 cookies={},
                 headers={}
             )
-            message_json = simplejson.dumps(message)
-            response = self.client.post(
-                path='/api/report',
-                data=message_json,
-                content_type='application/json',
-                headers={
-                    'X-Alpaca-Reporter': 'test1',
-                    'X-Alpaca-Signature': hmac.new(
-                        'test1_api_key', message_json, hashlib.sha256
-                    ).hexdigest(),
-                }
-            )
+            response = post_report(self.client, message, 'test1',
+                                   'test1_api_key')
             self.assertEqual(response.status_code, 200)
         occurrence_range = range(
             2,
@@ -196,6 +182,102 @@ class ApiTestCase(unittest.TestCase):
                     i
                 )
             )
+
+    def test_multiple_reporters(self):
+        def send_occurrence(reporter, api_key):
+            message = dict(
+                error_hash='error_hash_1',
+                traceback="Some traceback",
+                date=datetime.datetime.utcnow().isoformat(),
+                uri='',
+                get_data={},
+                post_data={},
+                cookies={},
+                headers={}
+            )
+            response = post_report(self.client, message, reporter, api_key)
+            self.assertEqual(response.status_code, 200)
+        limit = self.application.config['ERROR_OCCURRENCE_HISTORY_LIMIT']
+        for i in range(limit):
+            send_occurrence('test1', 'test1_api_key')
+        for i in range(7):
+            send_occurrence('test2', 'test2_api_key')
+        for i in range(3):
+            send_occurrence('test1', 'test1_api_key')
+        self.assertEqual(len(Error.objects), 1)
+        error = Error.objects[0]
+        self.assertItemsEqual(error.reporters, ['test1', 'test2'])
+        self.assertEqual(error.occurrence_counter, limit + 7 + 3)
+        self.assertEqual(len(error.occurrences), limit)
+        self.assertGreater(
+            error.occurrences[limit - 1].date,
+            max([o.date for o in error.occurrences[:limit - 2]])
+        )
+        self.assertEqual(error.occurrences[limit - 1].reporter, 'test1')
+        self.assertEqual(error.occurrences[limit - 2].reporter, 'test1')
+        self.assertEqual(error.occurrences[limit - 3].reporter, 'test1')
+        self.assertEqual(error.occurrences[limit - 4].reporter, 'test2')
+        self.assertEqual(error.occurrences[limit - 5].reporter, 'test2')
+        self.assertEqual(error.occurrences[limit - 6].reporter, 'test2')
+
+    def test_concurrent_reporting(self):
+        def send_occurrence(reporter, api_key):
+            message = dict(
+                error_hash='error_hash_1',
+                traceback="Some traceback",
+                date=datetime.datetime.utcnow().isoformat(),
+                uri='',
+                get_data={},
+                post_data={},
+                cookies={},
+                headers={}
+            )
+            response = post_report(self.client, message, reporter, api_key)
+            self.assertEqual(response.status_code, 200)
+        def reporter_thread(how_many, reporter, api_key):
+            for i in range(how_many):
+                send_occurrence(reporter, api_key)
+        threads = (
+            threading.Thread(target=reporter_thread, args=(
+                150, 'test1', 'test1_api_key',
+            )),
+            threading.Thread(target=reporter_thread, args=(
+                50, 'test1', 'test1_api_key',
+            )),
+            threading.Thread(target=reporter_thread, args=(
+                70, 'test2', 'test2_api_key',
+            )),
+            threading.Thread(target=reporter_thread, args=(
+                43, 'test2', 'test2_api_key',
+            )),
+            threading.Thread(target=reporter_thread, args=(
+                15, 'test1', 'test1_api_key',
+            )),
+            threading.Thread(target=reporter_thread, args=(
+                27, 'test2', 'test2_api_key',
+            )),
+            threading.Thread(target=reporter_thread, args=(
+                18, 'test2', 'test2_api_key',
+            )),
+        )
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        self.assertEqual(len(Error.objects), 1)
+        error = Error.objects[0]
+        self.assertItemsEqual(error.reporters, ['test1', 'test2'])
+        self.assertEqual(
+            error.occurrence_counter,
+            150 + 50 + 70 + 43 + 15 + 27 + 18
+        )
+        self.assertEqual(
+            len(error.occurrences),
+            min(
+                150 + 50 + 70 + 43 + 15 + 27 + 18,
+                self.application.config['ERROR_OCCURRENCE_HISTORY_LIMIT']
+            )
+        )
 
 if __name__ == '__main__':
     unittest.main()
