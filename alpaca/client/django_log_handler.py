7eea2da78b400471f57d46e273b9c058e6ded0b0
Mikołaj Siedlarek <m.siedlarek@nctz.net>
2012-06-14 10:43:55 +0200

    mangling potential passwords from stack trace

--- alpaca/client/django_log_handler.py
+++ alpaca/client/django_log_handler.py
@@ -27,8 +27,26 @@ from django.views.debug import SafeExceptionReporterFilter
 logger = logging.getLogger('alpaca')
 
 class ForcedSafeExceptionReporterFilter(SafeExceptionReporterFilter):
+
     def is_active(self, request):
         return True
+
+    def get_traceback_frame_variables(self, *args, **kwargs):
+        variables = super(ForcedSafeExceptionReporterFilter, self)\
+                        .get_traceback_frame_variables(*args, **kwargs)
+        return [
+            (
+                key,
+                (
+                    value
+                    if not key.lower().startswith('password')
+                        and not key.lower().startswith('passwd')
+                    else
+                    '*****'
+                )
+            ) for key, value in variables
+        ]
+
 exception_reporter_filter = ForcedSafeExceptionReporterFilter()
 
 def send_alpaca_report(url, reporter, api_key, message, ca_bundle=None):
