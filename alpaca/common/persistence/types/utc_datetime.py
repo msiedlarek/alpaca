fc434773f14dc8a3370ec0ca7e2b48ebe4556682
Mikołaj Siedlarek <msiedlarek@nctz.net>
2013-10-12 20:28:49 +0200

    remove tzinfo from saved datetime

--- alpaca/common/persistence/types/utc_datetime.py
+++ alpaca/common/persistence/types/utc_datetime.py
@@ -10,7 +10,7 @@ class UTCDateTime(types.TypeDecorator):
     def process_bind_param(self, value, engine):
         if value is None:
             return None
-        return value.astimezone(pytz.utc)
+        return value.astimezone(pytz.utc).replace(tzinfo=None)
 
     def process_result_value(self, value, engine):
         if value is None:
